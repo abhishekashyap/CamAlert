7bf5c256c198c26081767b9094fe401dde13b825
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2019-09-16 20:06:58 +0530

    Webcam image rendering

--- DesktopApp/server.js
+++ DesktopApp/server.js
@@ -6,6 +6,10 @@ const device = require("express-device");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+
+const wCap = new cv.VideoCapture(0);
+
+// Getting local Ip 
 var ip;
 require("dns").lookup(require("os").hostname(), (err, add, fam) => {
   getIp(add);
@@ -18,31 +22,31 @@ function getIp(add) {
 
 
 // app.use("/assets", express.static(__dirname + '/assets'));
-app.use(device.capture());
-app.get("/hello", function(req, res) {
-  res.send("Hi to " + req.device.type + " User");
-});
+// app.use(device.capture());
+// app.get("/hello", function(req, res) {
+//   res.send("Hi to " + req.device.type + " User");
+// });
 
 // var device_type;
 // if (device_type == "mobile") {
 //}
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   console.log("accessed");
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, 'index.html'));
   //device_type = req.device.type;
   
 });
 
 
-const cam = new cv.VideoCapture(0);
+
 setInterval(() => {
-  const frame = cam.read();
-  const image = cv.imencode(".jpg", frame).toString("base64");
+  const frame = wCap.read();
+  const image = cv.imencode(".jpg", frame).toString('base64');
   io.emit("image", image);
 }, 1000);
 
 
-console.log(typeof(ip));
-// app.listen(3000);
+//console.log(typeof(ip));
+ app.listen(3000);
 // Listening at LAN as well as well as localhost
-app.listen(3000, ip || "127.0.0.1");
+//app.listen(3000, ip || "127.0.0.1");
