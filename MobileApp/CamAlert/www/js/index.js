f44e222ce1138cda05fd7a7dbf7a45d2fa6a9c06
Nischey <43901336+NB134@users.noreply.github.com>
2019-09-17 21:25:56 +0530

    Changed Css

--- MobileApp/CamAlert/www/js/index.js
+++ MobileApp/CamAlert/www/js/index.js
@@ -23,24 +23,108 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+
+    
 };
 
-// NavBar opening and closing
-function openNav() {
-    document.getElementById("sideMenu").style.width = "65vw";
-  }
-  window.onload = function() {
-    closeNav();
-  };  
+$(document).ready(function () {
 
-  function closeNav() {
-    window.addEventListener('mouseup',function(event){
-    var closeMenu = document.getElementById('sideMenu');
-    if(event.target != closeMenu && event.target.parentNode != closeMenu){       
-        document.getElementById("sideMenu").style.width = "0";
-    }
-    });  
-    document.getElementById("sideMenu").style.width = "0";
-}
+    /*|||||||||||||||||||||||||||||||||||--To-Open-Navbar--|||||||||||||||||||||||||||||||||||||*/
+    $("#NavButton").click(function () {
+        $("#sideMenu a").show();
+        $("#sideMenu").animate({
+            width:'240px'
+        });
+        $("#nav-container").css("display","flex");
+       
+    });
+
+
+    /*|||||||||||||||||||||||||||||||||||--close-Navbar-Through-closeButton-|||||||||||||||||||||||||||||||||||||*/
+    $(".closebtn").click(function()
+    {   $("#sideMenu a").hide(1000); // since the animation effect on close is not smooth so i made the text disappear to make it smooth any better fix is appreciated
+        $("#sideMenu").animate({
+            width:'0px'
+        }, function(){$("#nav-container").hide()});
+    });
+
+    /*|||||||||||||||||||||||||||||||||||--To-Close-Navbar-ByClicking-Outside-|||||||||||||||||||||||||||||||||||||*/
+    $("#navback").click(function () {
+        $("#sideMenu a").hide(1000);
+        $("#sideMenu").animate({
+            width:'0px'
+        }, function(){$("#nav-container").hide()});
+        
+    });
+    
+    /*|||||||||||||||||||||||||||||||||||--To-Goto-HomePage--|||||||||||||||||||||||||||||||||||||*/
+    $("#home").click(function(){
+        if($("#Mon").css("height")!='0px')
+        {
+            $("#Mon").animate({
+                height:"0vh"
+            });
+        }
+        else{
+            $("#Rec").animate({
+                height:"0vh"
+            });
+        }
+        $("#sideMenu a").hide(1000);
+        $("#sideMenu").animate({
+            width:'0px'
+        }, function(){$("#nav-container").hide()});
+    });
+
+    /*|||||||||||||||||||||||||||||||||||--To-Goto-RecordPane--|||||||||||||||||||||||||||||||||||||*/
+    $("#Record").click(function()
+    {
+        $("#Rec").animate({
+            height:'100vh'
+        });
+        if($("#Mon").css("height")!='0px')
+        {
+            $("#Mon").animate({
+                height:"0vh"
+            });
+        }
+        $("#sideMenu a").hide(1000);
+        $("#sideMenu").animate({
+            width:'0px'
+        }, function(){$("#nav-container").hide()});
+    });
+    /*|||||||||||||||||||||||||||||||||||--To-Goto-MonitorPane--|||||||||||||||||||||||||||||||||||||*/
+    $("#Monitor").click(function()
+    {
+        $("#Mon").animate({
+            height:'100vh'
+        });
+        if($("#Rec").css("height")!='0px')
+        {
+            $("#Rec").animate({
+                height:"0vh"
+            });
+        }
+        $("#sideMenu a").hide(1000);
+        $("#sideMenu").animate({
+            width:'0px'
+        }, function(){$("#nav-container").hide()});
+    });
+
+    /*|||||||||||||||||||||||||||||||||||--To-Open-RecordPane--|||||||||||||||||||||||||||||||||||||*/
+    $("#cont1").click(function(){
+        $("#Rec").animate({
+            height:'100vh'
+        });
+    });
+    /*|||||||||||||||||||||||||||||||||||--To-Open-MonitorPane--|||||||||||||||||||||||||||||||||||||*/
+    $("#cont2").click(function(){
+        $("#Mon").animate({
+            height:'100vh'
+        }); 
+    });
+    
+    
+});
   
 app.initialize();
