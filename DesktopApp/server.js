4aa6c31786c68d91f02839f5772d79d59ecbca0b
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2019-09-18 01:47:11 +0530

    Added third feature- upload to serverside

--- DesktopApp/server.js
+++ DesktopApp/server.js
@@ -7,6 +7,9 @@ const device = require("express-device");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+const PORT = 3000;
+
+// Capturing from default camera i.e. webcam
 const wCap = new cv.VideoCapture(0);
 
 // Getting local Ip
@@ -20,20 +23,28 @@ function getIp(add) {
   console.log(ip);
 }
 
-// app.use('/assets', express.static(__dirname + '/assets'));
-// app.use(device.capture());
-// app.get('/hello', function(req, res) {
-//   res.send('Hi to ' + req.device.type + ' User');
-// });
+/*
+To check from what device server is accessed
 
-// var device_type;
-// if (device_type == 'mobile') {
-//}
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use(device.capture());
+app.get('/hello', function(req, res) {
+  res.send('Hi to ' + req.device.type + ' User');
+});
+var device_type;
+if (device_type == 'mobile') {
+}
+*/
+
+// SERVING HTML PAGES
+
 app.get("/", (req, res) => {
   console.log("accessed");
   res.sendFile(path.join(__dirname, "index.html"));
   //device_type = req.device.type;
 });
+
+// DOWNLOADING CAPTURED IMAGES
 
 var num = 0; // Used to initialize image number
 
@@ -54,6 +65,9 @@ setInterval(() => {
 }, 1000);
 
 //console.log(typeof(ip));
-server.listen(3000);
+server.listen(PORT, function() {
+  console.log("Express server listening on port ", PORT);
+});
+
 // Listening at LAN as well as well as localhost
 //app.listen(3000, ip || '127.0.0.1');
